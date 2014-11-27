64e05d6a5c90f0ddd84ab2f7260b866685775393
Ambroise Christea <ambroise.christea@cloudwatt.com>
2014-11-27 16:03:36 +0100

    Post on config now return added object's id

--- configs/views.py
+++ configs/views.py
@@ -1,7 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
 from configs.models import Config
 from configs.serializers import ConfigSerializer
 
 class ConfigViewSet(viewsets.ModelViewSet):
     queryset = Config.objects.all()
     serializer_class = ConfigSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.DATA, files=request.FILES)
+
+        if serializer.is_valid():
+            self.pre_save(serializer.object)
+            self.object = serializer.save(force_insert=True)
+            self.post_save(self.object, created=True)
+            headers = self.get_success_headers(serializer.data)
+            return Response(self.object.id, status=status.HTTP_201_CREATED,
+                            headers=headers)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
