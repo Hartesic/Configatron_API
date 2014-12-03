7604683346fa7c47bb868fae3fdd4a5cc13583fc
Ambroise Christea <ambroise.christea@cloudwatt.com>
2014-12-03 15:20:41 +0100

    Fix permissions

--- configs/views.py
+++ configs/views.py
@@ -1,11 +1,13 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from configs.models import Config
 from configs.serializers import ConfigSerializer
 
 class ConfigViewSet(viewsets.ModelViewSet):
     queryset = Config.objects.all()
     serializer_class = ConfigSerializer
+    permission_classes = [AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.DATA, files=request.FILES)
