a9d304232cb801b9c4d484b4d89578bf5f9eb13e
Ambroise Christea <ambroise.christea@cloudwatt.com>
2014-12-11 19:28:03 +0100

    Fix Debug setting

--- Configatron_API/settings.py
+++ Configatron_API/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'r&5kzxgn4qkg=2a6@2)_rq*+d9ekeg#6$=4=sxpnsu$otbq!xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
